e451574b5bec43e731a867b8d7b1f74f211aa32e
Tesskhan <avesquius05@gmail.com>
2025-02-12 12:21:35 +0100

    Speed up power up

---
+++ foodie_map/src/app/components/Layout.js
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar";
+import styles from "./Layout.module.css";
+
+export default function Layout({ children }) {
+  return (
+    <div className={styles.layout}>
+      <Sidebar />
+      <main className={styles.mainContent}>{children}</main>
+    </div>
+  );
+}

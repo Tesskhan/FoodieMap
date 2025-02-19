b218ae1e5322be81bcce36a6bab94ab8f3f86df4
Tesskhan <avesquius05@gmail.com>
2025-02-19 08:22:39 +0100

    Reapply "Speed up power up"
    This reverts commit 78b830789cb23aeba75b7d2edcccdc60c935c6f8.

---
+++ foodie_map/src/app/pages/edit_restaurants.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function EditReviewers() {
+    return (
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Edit Restaurants</h1>
+        <p>You are on the Edit Restaurants page.</p>
+      </div>
+    );
+  }
+  
---
+++ foodie_map/src/app/pages/edit_reviewers.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function EditReviewers() {
+    return (
+      <div className="p-6">
+        <h1 className="text-2xl font-bold">Edit Reviewers</h1>
+        <p>You are on the Edit Reviewers page.</p>
+      </div>
+    );
+  }
+  
